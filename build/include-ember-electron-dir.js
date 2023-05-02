2038b8fea03a603f02798358f967e88a7328ff1b
Sam Power <samanthakpower@gmail.com>
2023-05-02 18:43:42 -0400

    ember-electron upgrade: use electron-app folder

--- build/include-ember-electron-dir.js
+++ build/include-ember-electron-dir.js
@@ -2,7 +2,7 @@ const path = require('path');
 const Funnel = require('broccoli-funnel');
 
 module.exports = function includeEmberElectronDir() {
-  return new Funnel(path.join(__dirname, '..', 'ember-electron'), {
-    destDir: 'ember-electron'
+  return new Funnel(path.join(__dirname, '..', 'electron-app'), {
+    destDir: 'electron-app'
   });
 };
