07935614d5a68997909da194eef530543ad6382f
Sam Power <samanthakpower@gmail.com>
2023-05-02 19:06:22 -0400

    wip

--- build/include-ember-electron-dir.js
+++ build/include-ember-electron-dir.js
@@ -2,7 +2,7 @@ const path = require('path');
 const Funnel = require('broccoli-funnel');
 
 module.exports = function includeEmberElectronDir() {
-  return new Funnel(path.join(__dirname, '..', 'electron-app'), {
+  return new Funnel(path.join(__dirname, '/..', 'src', 'initializers'), {
     destDir: 'electron-app'
   });
 };
